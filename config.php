0c21f7aca0270722f20b74f59505098e7e7076dd
NetJunior180 <netjuni0r180@gmail.com>
2024-05-21 12:40:50 -0500

    Update config

--- config.php
+++ config.php
@@ -1,16 +1,21 @@
 <?php
 
-$server       = getenv('DB_HOST');
-$user   = getenv('DB_USERNAME');
-$password   = getenv('DB_PASSWORD');
-$nama_database     = getenv('DB_DATABASE');
-$sslcert    = "DigiCertGlobalRootCA.crt.pem";
+$server = getenv('DB_HOST');
+$user = getenv('DB_USERNAME');
+$password = getenv('DB_PASSWORD');
+$nama_database = getenv('DB_DATABASE');
+$sslcert = "DigiCertGlobalRootCA.crt.pem";
 
-$db = mysqli_connect($server, $user, $password, $nama_database);
+$conn = mysqli_init();
 
-//Establish the connection
-        $conn = mysqli_init();
-        mysqli_ssl_set($conn,NULL,NULL,$sslcert,NULL,NULL);
-        if(!mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL)){
-            die('Failed to connect to MySQL: '.mysqli_connect_error());
-        }
+// Configurar la conexión SSL
+mysqli_ssl_set($conn, NULL, NULL, $sslcert, NULL, NULL);
+
+// Intentar establecer la conexión
+if (!mysqli_real_connect($conn, $server, $user, $password, $nama_database, 3306, NULL, MYSQLI_CLIENT_SSL)) {
+    die('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+
+// Si la conexión es exitosa
+echo 'Successfully connected to MySQL with SSL!';
+?>
